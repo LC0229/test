941ee8dea09735c59ac7067f568d9fb588a801f2
LC0229 <shc067@ucsd.edu>
2026-01-02 01:25:10 -0800

    test: Add test file to trigger workflow

---
+++ test_trigger.py
@@ -0,0 +1,12 @@
+"""
+Test file to trigger auto-documentation workflow.
+This file is created to test the moxi auto-documentation feature.
+"""
+
+def test_function():
+    """A simple test function."""
+    print("This is a test function to trigger the workflow")
+    return "test"
+
+if __name__ == "__main__":
+    test_function()
